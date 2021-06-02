fba1f490ff7ccca6f6d147dad8afc7d308221482
JasonRillera <jarillera@gmail.com>
2021-06-02 12:41:04 -0700

    v1 done

---
+++ keys.js
@@ -0,0 +1,7 @@
+// https://developers.google.com/custom-search/v1/using_rest
+export const API_KEY = "AIzaSyB1z8hsi5IFBH22Gj8dk3kFzF4x8pwlsC0";
+
+// https://cse.google.com/cse/create/new
+export const CONTEXT_KEY = "9327c13cb34bc329f"; 
+
+
