a988ae0f1146c36f2c1513a132b4c345cde45c07
JasonRillera <jarillera@gmail.com>
2021-06-30 23:45:05 -0700

    avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,9 +1,9 @@
-function Avatar({ url, className }) {
+function Avatar({ className }) {
     return (
         <img 
             loading="lazy" 
             className={`h-10 rounded-full cursor-pointer transition duration-150 ease-out transform hover:scale-110 ${className}`}
-            src={url}
+            src="/GoogleAvatar.png"
             alt="profile pic"
         />
             
