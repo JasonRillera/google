6be57fdca7731e89728973ef08529f9aaacadaac
JasonRillera <jarillera@gmail.com>
2021-07-10 13:53:26 -0700

    update

--- keys.js
+++ keys.js
@@ -1,5 +1,5 @@
 // https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyB1z8hsi5IFBH22Gj8dk3kFzF4x8pwlsC0";
+export const API_KEY = "AIzaSyC0OnPAJAxkqhpV2OisJKAoSVElCTDM2j0";
 
 // https://cse.google.com/cse/create/new
 export const CONTEXT_KEY = "e51427c05cfd32719"; 
